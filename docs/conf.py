ef197603be15ee0a52026217cfd2f18433dba92d
GeraGC <92044973+GeraGC@users.noreply.github.com>
2025-09-01 20:52:30 +0200

    more readthedocs fixes

--- docs/conf.py
+++ docs/conf.py
@@ -52,6 +52,7 @@ epub_show_urls = 'footnote'
 # Override some CSS settings
 html_css_files = ["readthedocs-custom.css"]
 
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
