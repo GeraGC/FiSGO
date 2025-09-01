23254873d06394d4c027ec3b0a4bb2b61d353135
GeraGC <92044973+GeraGC@users.noreply.github.com>
2025-09-01 18:10:51 +0200

    readthedocs test fix requirements.txt

--- docs/conf.py
+++ docs/conf.py
@@ -1,6 +1,6 @@
 import os
 import sys
-sys.path.insert(0, os.path.abspath(os.path.join('..', 'FiSGO'))) # Source code dir relative to this file
+sys.path.insert(0, os.path.abspath('..')) # Source code dir relative to this file
 
 # Configuration file for the Sphinx documentation builder.
 # -- Project information
